fd9eb3f686351e0c9d640548b3fe9b913aa3d251
Martin Thierer <mthierer@gmail.com>
2020-04-04 21:30:27 +0200

    Linux: Define __int32 and min() needed by bitshifter.c

--- include/LINUX/mnibarch.h
+++ include/LINUX/mnibarch.h
@@ -8,3 +8,10 @@
 #define ARCH_SIGNALDECL
 
 typedef unsigned char BYTE;
+
+// __int32 and min() are needed for bitshifter.c (included by fileio.c)
+typedef int32_t __int32;
+
+#ifndef min
+#define min(x, y) ((x) > (y) ? (y) : (x))
+#endif
